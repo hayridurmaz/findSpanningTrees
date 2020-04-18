6dfcded1c9ab8c87ee2bb99139a092d284e42386
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2020-04-18 14:34:56 +0300

    some bugfixes, but stil buggy :(

--- Graph.py
+++ Graph.py
@@ -1,6 +1,7 @@
 from heapq import *
 
 import bcolors
+import numpy
 from heapdict import heapdict
 from DisjointSet import DisjointSet
 from Edge import Edge
@@ -15,16 +16,20 @@ class Graph:
         self.MST = []
         self.MST_weight = 0
 
+    def initializeGraph(self, graph):
+        self.numVertices = 0
+        self.edges = []
+        self.adjMatrix = []
+        self.original_edges = []
+        self.MST = []
+        self.MST_weight = 0
+        self.numVertices = graph.numVertices
+        self.edges[:] = graph.edges
+        self.adjMatrix[:] = graph.adjMatrix
+        self.original_edges[:] = graph.original_edges
+
     def __lt__(self, other):
         return self.MST_weight < other.MST_weight
-
-    def initializeGraph(self, graph):
-        self.numVertices = graph.numVertices
-        self.edges = graph.edges
-        self.adjMatrix = graph.adjMatrix
-        self.original_edges = graph.original_edges
-        self.MST = graph.MST
-        self.MST_weight = graph.MST_weight
 
     def initAdjMatris(self):
         for i in range(self.numVertices):
@@ -37,15 +42,9 @@ class Graph:
         self.adjMatrix[u][v] = 1
         self.adjMatrix[v][u] = 1
 
-    def modifyWeight(self, edge, weight):
-        for e in self.edges:
-            if e.u == edge.u & e.v == edge.v:
-                e.w = weight
-                break
-
     def containsCycle(self, visited_list):
         for e in self.edges:
-            if visited_list[e.u] & visited_list[e.v]:
+            if visited_list[e.u] and visited_list[e.v]:
                 return True
             visited_list[e.u] = True
             visited_list[e.v] = True
@@ -61,28 +60,6 @@ class Graph:
                 return False
         return True
 
-    def kruskalMST(self):
-        self.MST_weight = 0
-        sumOfWeights = 0
-        self.edges.sort(key=lambda x: x.w, reverse=False)
-        ds = DisjointSet(self.numVertices)
-        for e in self.edges:
-            u = e.u
-            v = e.v
-            set_of_u = ds.Find(u)
-            set_of_v = ds.Find(v)
-            if set_of_u != set_of_v:
-                w = e.w
-                self.MST.append(Edge(u, v, w))
-                sumOfWeights += w
-                ds.Union(set_of_u, set_of_v)
-
-        edges_in_MST = filter(lambda x: x in self.original_edges, self.MST)
-        for e in edges_in_MST:
-            self.MST_weight += e.w
-
-        return sumOfWeights
-
         # TODO: Burda kaldın
 
     def printGraph(self):
@@ -95,47 +72,83 @@ class Graph:
         for e in self.MST:
             print(str(e.u) + " -> " + str(e.v) + " (w:" + str(e.w) + ")")
 
-    def remove(self, arr, i):
-        # slicing till ith character
-        a = arr[: i]
-        # slicing from i+1th index
-        b = arr[i + 1:]
-        return a + b
-
     def printAllST(self):
         partitions = []
-        self.kruskalMST()
-        heappush(partitions, (self.MST_weight, self))
+        counter = 0
+        weight = kruskalMST(self)
+        heappush(partitions, self)
         while len(partitions) > 0:
-            smallest = heappop(partitions)[1]
+            smallest = heappop(partitions)
+            print(counter)
+            counter += 1
             smallest.printMST()
-            print("Weight of MST is: " + str(smallest.MST_weight))
+            print("Weight of MST is: " + str(weight))
+            print()
             i = 0
             for e in smallest.MST:
                 if e.w == 0:
-                    i += 1
+                    i = i + 1
                 else:
                     break
-            for j in range(0, self.numVertices - 1 - i):
+            for j in range(0, smallest.numVertices - 1 - i):
                 part = Graph()
                 part.initializeGraph(smallest)
-                part.MST_weight = part.kruskalMST()
 
-                for z in range(j, i + j):
+                for z in range(i, i + j):
                     for parts_edge in part.edges:
-                        if smallest.MST.get(z).u == parts_edge.u & smallest.MST.get(z).v == parts_edge.v:
-                            part.modifyWeight(parts_edge, 0)
+                        if smallest.MST[z].u == parts_edge.u and smallest.MST[z].v == parts_edge.v:
+                            modifyWeight(part, parts_edge, 0)
                             break
 
                 for z in range(0, len(part.edges)):
-                    if smallest.MST[(i + j)].u == part.edges[z].u & smallest.MST[(i + j)].v == part.edges[z].v:
-                        self.remove(part.edges, z)
+                    if smallest.MST[(i + j)].u == part.edges[z].u and smallest.MST[(i + j)].v == part.edges[z].v:
+                        part.edges = remove(part.edges, z)
                         break
 
                 if part.isConnected():
-                    part.kruskalMST()
+                    kruskalMST(part)
                     if len(part.MST) < part.numVertices - 1:
                         continue
-                    heappush(partitions, (part.MST_weight, part))
+                    heappush(partitions, part)
                 else:
                     continue
+
+
+def remove(arr, i):
+    # slicing till ith character
+    a = arr[: i]
+    # slicing from i+1th index
+    b = arr[i + 1:]
+    return a + b
+
+
+def kruskalMST(self):
+    self.MST_weight = 0
+    sumOfWeights = 0
+    self.edges.sort(key=lambda x: x.w, reverse=False)
+    ds = DisjointSet(self.numVertices)
+    for e in self.edges:
+        u = e.u
+        v = e.v
+        set_of_u = ds.Find(u)
+        set_of_v = ds.Find(v)
+        if set_of_u != set_of_v:
+            w = e.w
+            self.MST.append(Edge(u, v, w))
+            sumOfWeights += w
+            ds.Union(set_of_u, set_of_v)
+
+    for mst_edge in self.MST:
+        for original_edge in self.original_edges:
+            if original_edge.u == mst_edge.u and original_edge.v == mst_edge.v:
+                self.MST_weight = self.MST_weight + original_edge.w
+                break
+
+    return sumOfWeights
+
+
+def modifyWeight(self, edge, weight):
+    for e in self.edges:
+        if e.u == edge.u and e.v == edge.v:
+            e.w = weight
+            break
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # FindingAllSpanningTrees
- 
+This code generates all spanning trees of a graph, using [algorithm from Sorensen and Jannsen](http://www.scielo.br/pdf/pope/v25n2/25707.pdf)
